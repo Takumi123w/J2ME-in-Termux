e5af68379b324081b1cec7e4100ea0f67b0e6f4b
TakuruKagami <166905091+Takumi123w@users.noreply.github.com>
2026-06-15 14:07:08 +0700

    Add compile script for Java and ECJ setup

---
+++ compile.sh
@@ -0,0 +1,20 @@
+apt update
+if javac -version &> /dev/null
+then
+    echo "JDK 17 already installed"
+else
+    echo "Install openjdk 17"
+    apt install openjdk-17
+fi
+if ecj -version &> /dev/null
+    echo "ECJ already installed"
+else
+    echo "Install ECJ"
+    sudo apt install -y ecj
+fi
+ecj -source 1.3 -target 1.3 -bootclasspath lib/cldcapi11.jar:lib/midpapi20.jar Midlet.java
+mkdir -p compiled_classes preverified_classes
+mv AMIDlet.class compiled_classes/
+java -cp proguard.jar proguard.ProGuard @preverify.pro
+cd preverified_classes
+jar cvfm ../HelloApp.jar ../MANIFEST.MF AMIDlet.class
